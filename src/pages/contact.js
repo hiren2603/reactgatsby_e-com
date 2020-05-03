5aceb508e359b1a220802761db53feba04a1efe4
hiren2603 <hiren2603@users.noreply.github.com>
2020-05-03 18:11:55 +0530

    contact form ready

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,27 +5,26 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import HeroSection from "../components/Reusable/HeroSection"
 import Infoblock from "../components/Reusable/Infoblock"
-import Dualinfoblock from "../components/Reusable/Dualinfoblock"
-import Teamphotosection from "../components/About/Teamphotosection"
+import Contact from "../components/Contact/Contact"
 
-const AboutPage = ({ data }) => (
+
+const ContactPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <HeroSection
       img={data.img.childImageSharp.fluid}
-      title="About LearnCodeOnline"
+      title="Contact Us"
       subtitle=""
       heroclass="about-background"
-    />
-    <Dualinfoblock heading="A Message from CEO" src="src" />
-    <Infoblock heading="Our Vision" />
-    <Teamphotosection />
+    />  
+    <Infoblock heading="How can we help?" />
+    <Contact />
   </Layout>
 )
 
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "about.png" }) {
+    img: file(relativePath: { eq: "contact.png" }) {
       childImageSharp {
         fluid(maxWidth: 400, maxHeight: 250) {
           ...GatsbyImageSharpFluid_tracedSVG
@@ -34,4 +33,4 @@ export const query = graphql`
     }
   }
 `
-export default AboutPage
+export default ContactPage
